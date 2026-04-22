a5477779a54584d3f7706033d609e333b451b0cf
volgasmirnova2008-hub <volgasmirnova2008@gmail.com>
2026-04-22 20:01:29 +0300

    Update lab2var10.py

--- lab2var10/lab2var10.py
+++ lab2var10/lab2var10.py
@@ -2,7 +2,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 import os
 
-def sequences_to_protein_fasta(filename): #IN1.fasta IN 2.fasta IN3.fasta
+def sequences_to_protein_fasta(filename): #IN1.fasta IN2.fasta IN3.fasta
     results = []
     try:
         for record in SeqIO.parse(filename, "fasta"):
@@ -33,21 +33,18 @@ def sequences_to_protein_fasta(filename): #IN1.fasta IN 2.fasta IN3.fasta
     except Exception as e: 
         print(f"ошибка, проверьте последовательность: {e}")
 
-protein = sequences_to_protein_fasta('IN 2.fasta')
+protein = sequences_to_protein_fasta('IN2.fasta')
 print(protein)
 with open ('OUT 2.txt','r') as file:
     check = file.read()
 print(check == protein)
 
-
+#тест ошибки
 protein = sequences_to_protein_fasta('dna.fasta') #есть не нуклеотидные буквы
 protein = sequences_to_protein_fasta('dna2.fasta') #только днк
 
 
 
-# 
-os.listdir()
-os.getcwd()
 
 
 
